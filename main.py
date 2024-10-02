f14a8be5c617da3e379db9582db6b0618d2781ee
lmadanhire <37793681+lastmadanhire@users.noreply.github.com>
2024-10-02 15:31:27 +0200

    Update main.py
    uv

--- main.py
+++ main.py
@@ -19,6 +19,12 @@ from fastapi.responses import FileResponse
 from twilio.twiml.messaging_response import MessagingResponse
 from fastapi import FastAPI, Request, Response
 import json
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
 
 
 
